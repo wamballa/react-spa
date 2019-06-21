4e6f6c76fbe5ecc167d59eb4f91280625fc926cb
wamballa <steve.tyrell@gmail.com>
2019-06-21 20:23:19 +0100

    updating

---
+++ UTILS.md
@@ -0,0 +1,3 @@
+React tree hirearchy
+
+https://medium.com/bpxl-craft/introducing-react-component-hierarchy-35d5cc4d3782
--- src/components/layouts/SignedInLinks.js
+++ src/components/layouts/SignedInLinks.js
@@ -8,7 +8,9 @@ const SignedInLinks = (props) => {
   return (
     <ul className="right">
       <li><NavLink to='/create'>New Project</NavLink></li>
-      <li><a onClick={props.signOut}>Log Out</a></li>
+      {/* <li><a onClick={props.signOut}>Log Out</a></li> */}
+      <li><a href='/' onClick={props.signOut}>Log Out</a></li>
+      {/* <li><button onClick={props.signOut}>Log Out</button></li> */}
       <li><NavLink to='/' className="btn btn-floating pink lighten-1">
         {props.profile.initials}
       </NavLink></li>
